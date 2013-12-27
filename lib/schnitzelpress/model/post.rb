61c673ead1b70b1f494061bc874282e64dbe137c
MadCatme <madcat.me@gmail.com>
2013-12-27 23:28:21 +0100

    Add all necessary properties

--- lib/schnitzelpress/model/post.rb
+++ lib/schnitzelpress/model/post.rb
@@ -3,10 +3,18 @@ module Schnitzelpress
     class Post
       include DataMapper::Resource
 
-      property :id,           Serial
-      property :created_at,   DateTime
-      property :updated_at,   DateTime
-      property :published_at, DateTime
+      property :id,            Serial
+      property :published_at,  DateTime, :key => true
+      property :type,          Enum[:page, :post], :default => :post, :key => true
+      property :status,        Enum[:draft, :published], :default => :draft, :key => true
+      property :comments,      Boolean, :default => true
+      property :slug,          Slug, :key => true
+      property :title,         String, :key => true
+      property :body,          Text
+      property :rendered_body, Text
+
+      timestamps :at
+      timestamps :on
     end
   end
 end
