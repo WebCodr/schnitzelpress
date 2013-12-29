44aa94751886456fbd0015557e5029b4589750c2
David <david@webcodr.de>
2013-12-30 00:48:40 +0100

    Use DataMapper models instead of Mongoid

--- lib/schnitzelpress/actions/blog.rb
+++ lib/schnitzelpress/actions/blog.rb
@@ -8,50 +8,17 @@ module Schnitzelpress
           render_blog
         end
 
-        get '/blog/?' do
-          render_blog
-        end
-
         get '/blog.atom' do
           content_type 'application/atom+xml; charset=utf-8'
-          @posts = Post.latest.limit(10)
+          @posts = Schnitzelpress::Model::Post.latest.limit(10)
 
           slim :atom, :format => :xhtml, :layout => false
         end
 
-        get '/feed/?' do
-          redirect config.blog_feed_url, 307
-        end
-
-        get %r{^/(\d{4})/(\d{1,2})/(\d{1,2})/?$} do
-          year, month, day = params[:captures]
-          @posts = Post.latest.for_day(year.to_i, month.to_i, day.to_i)
-          render_posts
-        end
-
-        get %r{^/(\d{4})/(\d{1,2})/?$} do
-          year, month = params[:captures]
-          @posts = Post.latest.for_month(year.to_i, month.to_i)
-          render_posts
-        end
-
-        get %r{^/(\d{4})/?$} do
-          year = params[:captures].first
-          @posts = Post.latest.for_year(year.to_i)
-          render_posts
-        end
-
         get '/:year/:month/:day/:slug/?' do |year, month, day, slug|
-          @post = Post.
-            for_day(year.to_i, month.to_i, day.to_i).
-            where(:slugs => slug).first
+          @post = Schnitzelpress::Model::Post.for_day(year.to_i, month.to_i, day.to_i).all(:slug => slug).first
+          puts @post.inspect
 
-          render_post
-        end
-
-        get '/*/?' do
-          slug = params[:splat].first
-          @post = Post.where(:slugs => slug).first
           render_post
         end
 
@@ -60,10 +27,10 @@ module Schnitzelpress
         end
 
         def render_blog
-          @posts = Post.latest.skip(skipped).limit(10)
+          @posts = Schnitzelpress::Model::Post.latest.limit(10).skip(skipped)
 
-          displayed_count = @posts.count(true)
-          @show_previous_posts_button = Post.total > skipped + displayed_count
+          displayed_count = @posts.length
+          @show_previous_posts_button = Schnitzelpress::Model::Post.count > skipped + displayed_count
           @show_description = true
 
           render_posts
@@ -87,7 +54,7 @@ module Schnitzelpress
           end
 
           unless requested_canonical_url?(@post)
-            return redirect url_for(@post)
+            #return redirect url_for(@post)
           end
 
           slim :post
