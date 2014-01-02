cb3b7d49513650bf885591a8ca5823f92e507e39
David <david@webcodr.de>
2014-01-02 22:19:14 +0100

    Use DTOs in home action and presenter, add pagination

--- lib/schnitzelpress/action/home.rb
+++ lib/schnitzelpress/action/home.rb
@@ -7,11 +7,29 @@ module Schnitzelpress
       def call
         posts = Model::Post.latest.limit(10).skip(skipped)
 
-        success(posts)
+        page = DTO::Page.new(
+          current: current_page,
+          max: max_pages
+        )
+
+        dto = DTO::Home.new(
+          posts: posts,
+          page: page
+        )
+
+        success(dto)
       end
 
       def skipped
-        input.fetch('page', 0).to_i * 10
+        current_page * 10
+      end
+
+      def current_page
+        input.fetch('page', 0).to_i
+      end
+
+      def max_pages
+        (Schnitzelpress::Model::Post.posts.count / 10).ceil
       end
 
     end
--- lib/schnitzelpress/presenter/home.rb
+++ lib/schnitzelpress/presenter/home.rb
@@ -4,13 +4,29 @@ module Schnitzelpress
     class Home < self
 
       def posts
-        response.output.map do |post|
+        dto.posts.map do |post|
           Presenter::Post.new(post)
         end
       end
 
+      def previous_posts_button?
+        current_page < dto.page.max
+      end
+
+      def current_page
+        dto.page.current
+      end
+
+      def next_page
+        current_page + 1
+      end
+
       def page_title
         "Home | #{super}"
+      end
+
+      def dto
+        response.output
       end
 
     end
