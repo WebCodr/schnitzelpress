63858b85ad6f2a635d419b314f99c5a42c59c095
David <david@webcodr.de>
2014-01-02 23:22:03 +0100

    Add new action Post

---
+++ lib/schnitzelpress/action/post.rb
@@ -0,0 +1,32 @@
+module Schnitzelpress
+  class Action
+    # Post action
+    class Post < self
+
+      private
+
+      def call
+        post = Model::Post.for_day(*day).all(slug: slug).first
+
+        if post
+          success(post)
+        else
+          error(:post_not_found)
+        end
+
+      end
+
+      def day
+        [fetch_integer('year'), fetch_integer('month'), fetch_integer('day')]
+      end
+
+      def fetch_integer(name)
+        input.fetch(name, 0).to_i
+      end
+
+      def slug
+        input.fetch('slug', '')
+      end
+    end
+  end
+end
