9b547922d960833726a7c9f7ab4e880a8cec1d93
David <david@webcodr.de>
2014-01-02 18:39:15 +0100

    Add class Error

---
+++ lib/schnitzelpress/error.rb
@@ -0,0 +1,11 @@
+module Schnitzelpress
+  # Error handlers
+  class Error
+
+    include Adamantium::Flat, Concord::Public.new(:response)
+    extend Adamantium::Mutable
+
+    InternalError = Class.new(self)
+
+  end
+end
