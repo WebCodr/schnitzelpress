e4ade07483a4622cdc1067471320d0d7ab06394c
David <david@webcodr.de>
2014-01-03 17:17:42 +0100

    Remove old stuff and Capybara setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,21 @@
-SPEC_DIR = File.dirname(__FILE__)
-lib_path = File.expand_path("#{SPEC_DIR}/../lib")
-$LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 ENV['RACK_ENV'] = 'test'
 
-require 'rubygems'
 require 'bundler/setup'
 require 'schnitzelpress'
 require 'awesome_print'
-require 'rack/test'
-require 'rspec-html-matchers'
 require 'database_cleaner'
 require 'ffaker'
 require 'timecop'
 require 'factory_helper'
+require 'capybara'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.app = Schnitzelpress::App
+Capybara.javascript_driver = :poltergeist
+
 Timecop.freeze
 
 RSpec.configure do |config|
