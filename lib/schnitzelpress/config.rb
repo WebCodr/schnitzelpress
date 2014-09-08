afeafd961b2e14c16cb1608e104129a8e3c964be
David <david@webcodr.de>
2014-09-08 21:48:39 +0200

    Add configuration variable for tracking

--- lib/schnitzelpress/config.rb
+++ lib/schnitzelpress/config.rb
@@ -1,7 +1,7 @@
 module Schnitzelpress
   # Configuration class
   class Config
-    include Anima.new(:postgres_url, :developer_login)
+    include Anima.new(:postgres_url, :developer_login, :tracking)
 
     def self.load(environment)
       file = Schnitzelpress.config_path.join("#{environment}.yml")
@@ -9,10 +9,12 @@ module Schnitzelpress
 
       new(
         postgres_url:    config['postgres_url'],
-        developer_login: config['developer_login']
+        developer_login: config['developer_login'],
+        tracking:        config['tracking']
       )
     end
 
     alias_method :developer_login?, :developer_login
+    alias_method :tracking?, :tracking
   end
 end
