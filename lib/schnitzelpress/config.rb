67b130183cedb467c87c5ba27c79c2d4f12dc5df
David <david@webcodr.de>
2014-09-08 21:37:29 +0200

    Use config variable to enable/disable developer login

--- lib/schnitzelpress/config.rb
+++ lib/schnitzelpress/config.rb
@@ -1,13 +1,18 @@
 module Schnitzelpress
   # Configuration class
   class Config
-    include Anima.new(:postgres_url)
+    include Anima.new(:postgres_url, :developer_login)
 
     def self.load(environment)
       file = Schnitzelpress.config_path.join("#{environment}.yml")
       config = YAML.load(ERB.new(File.new(file).read).result)
 
-      new(postgres_url: config['postgres_url'])
+      new(
+        postgres_url:    config['postgres_url'],
+        developer_login: config['developer_login']
+      )
     end
+
+    alias_method :developer_login?, :developer_login
   end
 end
