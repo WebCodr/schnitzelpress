9f17d6baa609955575b8630baf45d2cbd343ec0d
David <david@webcodr.de>
2014-09-08 21:28:27 +0200

    Use default value
    Value 'test' hardcoded will cause trouble with Circle and Travis CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 SPEC_DIR = File.dirname(__FILE__)
 lib_path = File.expand_path("#{SPEC_DIR}/../lib")
 $LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
-ENV['SCHNITZEL_ENV'] = 'test'
+ENV['SCHNITZEL_ENV'] ||= 'test'
 
 require 'rubygems'
 require 'bundler/setup'
