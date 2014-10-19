9582f825066a30b40742d8eca3095c9eba70e1b4
David <david@webcodr.de>
2014-10-19 22:21:27 +0200

    Add documentation and rename instance methods #call to #response

--- lib/schnitzelpress/config.rb
+++ lib/schnitzelpress/config.rb
@@ -3,6 +3,12 @@ module Schnitzelpress
   class Config
     include Anima.new(:postgres_uri, :developer_login, :tracking)
 
+    # Return config object
+    #
+    # @return [Config]
+    #
+    # @api private
+    #
     def self.load(environment)
       Loader.call(environment)
     end
@@ -13,24 +19,54 @@ module Schnitzelpress
     class Loader
       include Concord.new(:environment)
 
+      # Return response
+      #
+      # @return [Config]
+      #
+      # @api private
+      #
       def self.call(environment)
-        new(environment).call
+        new(environment).response
       end
 
-      def call
+      # Return transformed config object
+      #
+      # @return [Config]
+      #
+      # @api private
+      #
+      def response
         Transformer.call(parsed_content)
       end
 
     private
 
+      # Return file path
+      #
+      # @return [Pathname]
+      #
+      # @api private
+      #
       def filepath
         Schnitzelpress.config_path.join("#{environment}.yml")
       end
 
+      # Return file content
+      #
+      # @return [String]
+      #
+      # @api private
+      #
       def file_content
         File.new(filepath).read
       end
 
+      # Parse file content with ERB und YAML and return hash
+      #
+      # @return [Hash]
+      #
+      # @api private
+      #
       def parsed_content
         parsed_erb_content = ERB.new(file_content).result
 
@@ -41,20 +77,46 @@ module Schnitzelpress
         include Concord.new(:hash)
         include Morpher::NodeHelpers
 
+        # Return response
+        #
+        # @return [Config]
+        #
+        # @api private
+        #
         def self.call(hash)
-          new(hash).call
+          new(hash).response
         end
 
-        def call
+        # Return transformed config object
+        #
+        # @return [Config]
+        #
+        # @api private
+        #
+        def response
           evaluator.call(hash)
         end
 
       private
 
+        # Return evaluator
+        #
+        # @return [Morpher::Evaluator]
+        #
+        # @api private
+        #
         def evaluator
-          Morpher.compile(nodes)
+          Morpher.compile(node_tree)
         end
 
+        # Return boolean AST node
+        #
+        # @param [Symbol] name
+        #
+        # @return [Parser::AST::Node]
+        #
+        # @api private
+        #
         def boolean_node(name)
           s(:key_symbolize, name,
             s(:or,
@@ -64,13 +126,28 @@ module Schnitzelpress
           )
         end
 
+        # Return string AST node
+        #
+        # @param [Symbol] name
+        #
+        # @return [Parser::AST::Node]
+        #
+        # @api private
+        #
         def string_node(name)
           s(:key_symbolize, name,
             s(:guard, s(:primitive, String))
           )
         end
 
-        def nodes
+
+        # Return AST node tree
+        #
+        # @return [Parser::AST::Node]
+        #
+        # @api private
+        #
+        def node_tree
           s(:block,
             s(:guard, s(:primitive, Hash)),
             s(:hash_transform,
